9afcb1ac985a549b7042f406f138e7459e192fc4
vKrains <vvorobev02@vk.com>
2024-06-06 12:45:33 +0300

    design.sv сам интерфейс apb

--- sim/src/tb.sv
+++ sim/src/tb.sv
@@ -1,5 +1,6 @@
 `timescale 1ns/1ps
 `include "../src/AXIS_UVM_Agent/src/axis_intf.sv"
+`include "uvm_apb_agent/design.sv"
 module tb;
     import uvm_pkg::*;
     import uvm_uart_test_pkg::*;
@@ -10,7 +11,7 @@ module tb;
     bit rst_n = 1;
 
     axis_if  u_axis_if (aclk);
-    uvma_apb_if u_apb_if (aclk, rst_n);
+    dut_if u_apb_if;
 
     initial begin
         uvm_config_db #(virtual axis_if)::set(null, "uvm_test_top.*", "mst_axis_if_h", u_axis_if);
--- sim/src/uvm_uart_test_pkg/uvm_uart_test_pkg.sv
+++ sim/src/uvm_uart_test_pkg/uvm_uart_test_pkg.sv
@@ -2,8 +2,19 @@ package uvm_uart_test_pkg;
 
     import uvm_pkg::*;
     `include "uvm_macros.svh"
-    `include "../uvma_apb/dv/uvma_apb/src/uvma_apb_pkg.sv"
-    // `include "../../src/AXIS_UVM_Agent/src/axis_intf.sv"
+    
+    `include "../uvm_apb_agent/apb_subsriber.sv"
+    `include "../uvm_apb_agent/apb_agent.sv"
+    `include "../uvm_apb_agent/apb_driver.sv"
+    `include "../uvm_apb_agent/apb_env.sv"
+    `include "../uvm_apb_agent/apb_monitor.sv"
+    `include "../uvm_apb_agent/apb_scoreboard.sv"
+    `include "../uvm_apb_agent/apb_sequence.sv"
+    `include "../uvm_apb_agent/apb_sequencer.sv"  
+    `include "../uvm_apb_agent/apb_subsriber.sv"
+    `include "../uvm_apb_agent/apb_test.sv" 
+    `include "../uvm_apb_agent/apb_transaction.sv" 
+
     `include "../../src/AXIS_UVM_Agent/src/axis_include.svh"
     `include "uvm_uart_env.sv"
     `include "uvm_uart_base_test.sv"
