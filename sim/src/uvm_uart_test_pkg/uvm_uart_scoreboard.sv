86c117397b794613f87eb24e86f1fe740484ad76
vKrains <vvorobev02@vk.com>
2024-06-11 15:45:21 +0300

    пофиксил scoreboard, теперь запускается

--- sim/src/uvm_uart_test_pkg/uvm_uart_scoreboard.sv
+++ sim/src/uvm_uart_test_pkg/uvm_uart_scoreboard.sv
@@ -12,11 +12,23 @@ class uvm_uart_scoreboard extends uvm_scoreboard;
     uvm_analysis_imp_slv_axis #(axis_data, uvm_uart_scoreboard)       analysis_port_slv_axis;
     //uvm_analysis_imp_mst_apb  #(apb_transaction, uvm_uart_scoreboard) analysis_port_mst_apb;
 
+
+    bit [8-1:0] axis_data_q_mst_axis[$];
+    bit [8-1:0] axis_data_q_slv_axis[$];
+
     virtual function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         analysis_port_mst_axis = new("analysis_port_mst_axis", this);
         analysis_port_slv_axis = new("analysis_port_slv_axis", this);
         //analysis_port_mst_apb =  new("analysis_port_mst_apb", this);
+    endfunction
+
+    function void write_mst_axis (axis_data axis_data_h);
+        axis_data_q_mst_axis.push_back(axis_data_h.tdata);
+    endfunction
+
+    function void write_slv_axis (axis_data axis_data_h);
+        axis_data_q_slv_axis.push_back(axis_data_h.tdata);
     endfunction
 
 endclass
